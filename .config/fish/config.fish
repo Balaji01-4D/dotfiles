05259415d6d5e18335ef34fa3d25f2df87bb620c
Balaji01-4D <j.balaji2468@gmail.com>
2025-12-10 19:34:38 +0530

    add fish config

---
+++ .config/fish/config.fish
@@ -0,0 +1,15 @@
+
+set -U fish_greeting ""
+
+if status is-interactive
+    oh-my-posh init fish --config ~/.oh-my-posh/themes/catppuccin_frappe.omp.json | source
+    zoxide init fish | source
+    alias cd='z'
+    alias tree='eza --tree --icons'
+    alias ls="eza --icons --group-directories-first"
+    alias ll="eza -l --icons --group-directories-first"
+    alias la="eza -la --icons --group-directories-first"
+    alias cat="bat --paging=never"
+
+end
+
