4addf5b025d01aeafb94396b2bfcb9ebceff114d
Balaji01-4D <j.balaji2468@gmail.com>
2025-12-10 20:19:45 +0530

    add wezterm config

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,54 @@
+local wezterm = require 'wezterm'
+
+wezterm.on('format-window-title', function(tab, pane, tabs, panes, config)
+	return 'WezTerm'
+end)
+
+return {
+	adjust_window_size_when_changing_font_size = false,
+	-- color_scheme = 'termnial.sexy',
+	color_scheme = 'Catppuccin Mocha',
+	enable_tab_bar = false,
+	font_size = 16.0,
+	font = wezterm.font("JetBrainsMono Nerd Font", {weight="Light", stretch="Normal", style="Normal"}),
+	-- macos_window_background_blur = 40,
+-- 	macos_window_background_blur = 30,
+	-- window_background_image = '/home/balaji-pc/.config/wezterm/pink-clouds.jpg',
+	window_background_image_hsb = {
+	
+		brightness = 0.20,
+		hue = 1.0,
+		saturation = 0.9,
+	},
+	-- window_background_opacity = 1.0,
+	-- window_background_opacity = 0.20,
+  -- window_background_opacity = 0.78,
+	 window_background_opacity = 0.92,
+	window_decorations = 'TITLE',
+	keys = {
+		{
+			key = 'q',
+			mods = 'CTRL',
+			action = wezterm.action.ToggleFullScreen,
+		},
+		{
+			key = '/',
+			mods = 'CTRL',
+			action = wezterm.action.SendString "clear\n", 
+		},
+		{
+			key = '\'',
+			mods = 'CTRL',
+			action = wezterm.action.ClearScrollback 'ScrollbackAndViewport',
+		},
+
+	},
+	mouse_bindings = {
+	  -- Ctrl-click will open the link under the mouse cursor
+	  {
+		event = { Up = { streak = 1, button = 'Left' } },
+		mods = 'CTRL',
+		action = wezterm.action.OpenLinkAtMouseCursor,
+	  },
+	},
+}
