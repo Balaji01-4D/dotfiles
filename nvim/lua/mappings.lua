7617831254a95fa1b532c667de51176941b3a852
Balaji01-4D <j.balaji2468@gmail.com>
2026-01-11 22:58:20 +0530

    feat: add new keymaps for telescope lsp

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -1,33 +1,21 @@
-
 require "nvchad.mappings"
 
 local map = vim.keymap.set
 
-map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
--- Insert empty line ABOVE
-map("n", "[", "O<Esc>", { desc = "insert empty line above" })
+map("n", "<leader>ls", "<cmd>Telescope lsp_document_symbols<CR>", {
+    desc = "LSP document symbols",
+})
 
--- Insert empty line BELOW
-map("n", "]", "o<Esc>", { desc = "insert empty line below" })
+map("n", "<leader>ld", "<cmd>Telescope lsp_definitions<CR>", {
+    desc = "LSP definitions",
+})
 
-------------------------------------------------
--- gopher.nvim (Go tools)
-------------------------------------------------
--- Tags
-map("n", "<leader>gt", ":GoTagAdd json<CR>", { desc = "Go: add json tags" })
-map("n", "<leader>gr", ":GoTagRm json<CR>", { desc = "Go: remove json tags" })
-map("n", "<leader>gT", ":GoTagClear<CR>", { desc = "Go: clear all tags" })
+map("n", "<leader>lws", "<cmd>Telescope lsp_dynamic_workspace_symbols<CR>", {
+    desc = "LSP definitions",
+})
 
--- if err != nil { ... }
-map("n", "<leader>ge", ":GoIfErr<CR>", { desc = "Go: insert if err" })
-
--- Tests
-map("n", "<leader>ga", ":GoTestAdd<CR>", { desc = "Go: add tests for func" })
-map("n", "<leader>gA", ":GoTestsAll<CR>", { desc = "Go: generate tests for file" })
-map("n", "<leader>gx", ":GoTestsExp<CR>", { desc = "Go: tests for exported" })
-
--- Misc useful ones (optional)
-map("n", "<leader>gm", ":GoMod<CR>", { desc = "Go: run go mod tidy/etc" })
-map("n", "<leader>gg", ":GoGenerate<CR>", { desc = "Go: go generate" })
+map("n", "<leader>lr", "<cmd>Telescope lsp_references<CR>", {
+    desc = "LSP definitions",
+})
