30c99d25dec73656e447258f0df221b447054b60
Balaji01-4D <j.balaji2468@gmail.com>
2026-01-12 20:00:27 +0530

    feat(fish): add abbr

---
+++ fish/conf.d/abbr.fish
@@ -0,0 +1,9 @@
+abbr -a gs "git status"
+abbr -a gc 'git commit -m ""'
+abbr -a ga "git add"
+abbr -a proj "cd ~/Project/"
+abbr -a config "cd ~/.config/"
+abbr -a rm "rm -rf"
+abbr -a lst "ls -T"
+abbr -a sss "sudo systemctl start"
+abbr -a ssS "sudo systemctl stop"
