87d5a1888088e681a3163e66d1a7a275a11ebfbe
Balaji J <j.balaji4200@gmail.com>
2026-01-21 22:16:47 +0530

    update nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,38 +1,58 @@
-vim.g.base46_cache = vim.fn.stdpath "data" .. "/base46/"
-vim.g.mapleader = " "
-vim.opt.clipboard = "unnamedplus"
-vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {desc = "Rename symbol"})
--- bootstrap lazy and all plugins
-local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
-
-if not vim.uv.fs_stat(lazypath) then
-  local repo = "https://github.com/folke/lazy.nvim.git"
-  vim.fn.system { "git", "clone", "--filter=blob:none", repo, "--branch=stable", lazypath }
+if vim.fn.has("nvim-0.8") == 0 then
+    error("Need Neovim 0.8+ in order to use this config")
 end
 
+for _, cmd in ipairs({ "git", "rg", { "fd", "fdfind" } }) do
+    local name = type(cmd) == "string" and cmd or vim.inspect(cmd)
+    local commands = type(cmd) == "string" and { cmd } or cmd
+    ---@cast commands string[]
+    local found = false
+
+    for _, c in ipairs(commands) do
+        if vim.fn.executable(c) == 1 then
+            name = c
+            found = true
+        end
+    end
+
+    if not found then
+        error(("`%s` is not installed"):format(name))
+    end
+end
+
+vim.opt.termguicolors = true -- enable 24-bit RGB colors
+-- Disable annoying deprecated message
+vim.deprecate = function() end
+
+-- bootstrap lazy and all plugins
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
+end
 vim.opt.rtp:prepend(lazypath)
 
-local lazy_config = require "configs.lazy"
+-- Load default configurations and plugins
+for _, source in ipairs({
+    "plugins",
+    "options",
+    "mappings",
+    "autocmds",
+}) do
+    local ok, fault = pcall(require, source)
+    if not ok then
+        vim.api.nvim_err_writeln("Failed to load " .. source .. "\n\n" .. fault)
+    end
+end
 
--- load plugins
-require("lazy").setup({
-  {
-    "NvChad/NvChad",
-    lazy = false,
-    branch = "v2.5",
-    import = "nvchad.plugins",
-  },
-
-  { import = "plugins" },
-}, lazy_config)
-
--- load theme
-dofile(vim.g.base46_cache .. "defaults")
-dofile(vim.g.base46_cache .. "statusline")
-
-require "options"
-require "autocmds"
-
-vim.schedule(function()
-  require "mappings"
-end)
+-- Load custom configurations
+local exist, custom = pcall(require, "custom")
+if exist and type(custom) == "table" and custom.configs then
+    custom.configs()
+end
