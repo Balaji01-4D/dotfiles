c8735778855e07e1020c2ae5cb027b165c62ed9b
Balaji J <j.balaji4200@gmail.com>
2026-01-25 08:11:20 +0530

    update commit
    Signed-off-by: Balaji J <j.balaji4200@gmail.com>

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -3,13 +3,12 @@ set -U fish_greeting ""
 if status is-interactive
     oh-my-posh init fish --config ~/.config/oh-my-posh/themes/catppuccin_frappe.omp.json | source
     zoxide init fish | source
-    alias cd='z'
     alias tree='eza --tree --icons'
     alias ls="eza --icons --group-directories-first"
     alias ll="eza -l --icons --group-directories-first"
     alias la="eza -la --icons --group-directories-first"
-    alias cat="bat -p --paging=never"
     alias v="nvim"
+    alias y="yazi"
 
     export EDITOR="nvim"
 
