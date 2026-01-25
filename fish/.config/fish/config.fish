011ebf150b232133249799b0679a1ac2ee9f3ddf
Balaji J <j.balaji4200@gmail.com>
2026-01-25 08:42:18 +0530

    update config
    Signed-off-by: Balaji J <j.balaji4200@gmail.com>

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -12,5 +12,15 @@ if status is-interactive
 
     export EDITOR="nvim"
 
+
+
 end
 
+function y
+	set tmp (mktemp -t "yazi-cwd.XXXXXX")
+	command yazi $argv --cwd-file="$tmp"
+	if read -z cwd < "$tmp"; and [ "$cwd" != "$PWD" ]; and test -d "$cwd"
+		builtin cd -- "$cwd"
+	end
+	rm -f -- "$tmp"
+end
